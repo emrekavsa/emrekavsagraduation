21e8a0bc0b3168f8fd7a37b20c384707039d4dc6
emrekavsa <emrekavsa@hotmail.com>
2026-04-20 00:55:24 +0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
-export default function Home() {
+import { supabase } from '@/lib/supabase'
+
+export default async function Home() {
+  const { data, error } = await supabase.from('polls').select('*')
+
+  if (error) {
+    return <h1>Error: {error.message}</h1>
+  }
+
   return (
     <div>
-      <h1>
-        Test
-      </h1>
+      <h1>connected!</h1>
+      <p>{data.length}</p>
     </div>
-  );
+  )
 }
